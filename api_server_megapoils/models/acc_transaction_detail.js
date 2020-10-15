989a70155c82f1c362f37544149c66e973d144a6
rudnovd <rudnov.d@gmail.com>
2020-10-15 14:11:52 +0500

    fix(acc_transaction_detail): use acc_transaction date instead parameters date

--- api_server_megapoils/models/acc_transaction_detail.js
+++ api_server_megapoils/models/acc_transaction_detail.js
@@ -3,14 +3,6 @@ const db = require("../dbcon");
 module.exports = (params, cb = () => {}) => {
   const parameters = params.getParams
 
-  if (parameters.length === 4) {
-    const date = new Date(parameters[3])
-    if (!(date instanceof Date && !isNaN(date))) {
-      cb("wrong date format", [])
-      return
-    }
-  }
-
   db.exec(`select * from dbo.acc_transaction(@account_num)`,
   [ { name : "account_num", value : parameters[2] } ], (err, rowCount, rows) => {
     if (!err && rowCount > 0) {
@@ -20,8 +12,8 @@ module.exports = (params, cb = () => {}) => {
       if (withoutPayAmount.length) {
         for (const transaction of withoutPayAmount) {
           const queryParams = [
-            {name: "account_num", value: transaction.account_num},
-            {name: "month", value: parameters.length > 3 ? parameters[3] : ""}
+            { name: "account_num", value: transaction.account_num },
+            { name: "month", value: new Date(transaction.trans_date).toISOString() }
           ]
 
           db.exec(`select * from dbo.acc_transaction_detail(@account_num, @month)`, queryParams, (err, rowCount, rows) => {
