88730a6f6cc216e23bd5c676127b0279ad172a5a
rudnovd <rudnov.d@gmail.com>
2020-10-15 18:09:34 +0500

    refactor(acc_transaction_details): refactor model

--- api_server_megapoils/models/acc_transaction_detail.js
+++ api_server_megapoils/models/acc_transaction_detail.js
@@ -1,41 +1,24 @@
 const db = require("../dbcon");
 
 module.exports = (params, cb = () => {}) => {
-  const parameters = params.getParams
+  let details = 0
 
   db.exec(`select * from dbo.acc_transaction(@account_num)`,
-  [ { name : "account_num", value : parameters[2] } ], (err, rowCount, rows) => {
-    if (!err && rowCount > 0) {
-      const withoutPayAmount = rows.filter(row => row.pay_amount === 0)
-      const response = []
+  [ { name : "account_num", value : params.getParams[2] } ], (err, rowCount, transactions) => {
+    if (!err) {
+      for (const transaction of transactions) {
+        if (transaction.pay_amount != 0) continue;
 
-      if (withoutPayAmount.length) {
-        for (const transaction of withoutPayAmount) {
-          const queryParams = [
-            { name: "account_num", value: transaction.account_num },
-            { name: "month", value: new Date(transaction.trans_date).toISOString() }
-          ]
+        const queryParams = [
+          { name: "account_num", value: transaction.account_num },
+          { name: "month", value: new Date(transaction.trans_date).toISOString() }
+        ]
+        db.exec(`select * from dbo.acc_transaction_detail(@account_num, @month)`, queryParams, (err, rowCount, transactionDetails) => {
+          transaction.detail = transactionDetails
 
-          db.exec(`select * from dbo.acc_transaction_detail(@account_num, @month)`, queryParams, (err, rowCount, rows) => {
-            if (!err && rowCount > 0) {
-              response.push({
-                ...transaction, 
-                detail: rows
-              })
-            } else {
-              response.push({
-                ...transaction,
-                detail: []
-              })
-            }
-            
-            if (response.length === withoutPayAmount.length) {
-              cb(err, response)
-            }
-          });
-        }
-      } else {
-        cb(err, rows)
+          details++
+          if (transactions.length - 1 === details) cb(err, transactions)
+        });
       }
     } else {
       cb(err,rows);
