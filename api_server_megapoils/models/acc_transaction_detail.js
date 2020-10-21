d068d082d879c4a7e35d97474810b626ff6fac84
rudnovdm <rudnovdm@ds24.ru>
2020-10-21 16:17:12 +0500

    fix(acc_transaction_detail): clear

--- api_server_megapoils/models/acc_transaction_detail.js
+++ api_server_megapoils/models/acc_transaction_detail.js
@@ -31,46 +31,9 @@ module.exports = async (params, cb = () => {}) => {
   
       transaction.detail = await accTransactionDetail(queryParams)
     }
-    
+
     cb('', transactions)
   } catch (err) {
     cb(err, [])
   }
-
-
-
-  // db.exec(`select * from dbo.acc_transaction(@account_num)`,
-  // [ { name : "account_num", value : params.getParams[2] } ], async (err, rowCount, transactions) => {
-  //   if (!err && rowCount) {
-  //     for (const transaction of transactions) {
-  //       // details++
-  //       if (transaction.pay_amount != 0) continue;
-
-  //       const queryParams = [
-  //         { name: "account_num", value: transaction.account_num },
-  //         { name: "month", value: new Date(transaction.trans_month).toISOString() }
-  //       ]
-
-  //       const r = await accTransactionDetail(queryParams)
-  //       transactions.detail = r
-  //       console.log(transactions.detail)
-
-  //       // db.exec(`select * from dbo.acc_transaction_detail(@account_num, @month)`, queryParams, (err, rowCount, transactionDetails) => {
-  //       //   if (err) {
-  //       //     cb(err, []);
-  //       //     return;
-  //       //   }
-  //       //   transaction.detail = transactionDetails
-
-  //       //   console.log(transactions.length, details)
-  //       //   if (transactions.length - 1 === details) cb(err, transactions)
-  //       // });
-  //     }
-
-  //     // if (transactions.length - 1 === details) cb(err, transactions)
-  //     cb(err, transactions)
-  //   } else {
-  //     cb(err, transactions);
-  //   }
-  // });
 }
